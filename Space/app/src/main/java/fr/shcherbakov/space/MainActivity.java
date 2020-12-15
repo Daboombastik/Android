10dec22c9c392ebff3329777f6d6f65c50d92603
Sergey Shcherbakov <s.shcherbakov@yahoo.fr>
2020-12-15 10:12:26 +0100

    Main commit

--- Space/app/src/main/java/fr/shcherbakov/space/MainActivity.java
+++ Space/app/src/main/java/fr/shcherbakov/space/MainActivity.java
@@ -1,13 +1,41 @@
 package fr.shcherbakov.space;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.view.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import fr.shcherbakov.space.engine.Engine;
 
 public class MainActivity extends AppCompatActivity {
+
+    SurfaceView surface;
+    Engine engine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null)
+                controller.hide(WindowInsets.Type.statusBars());
+        }
+        else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        surface = findViewById(R.id.surface);
+        engine = new Engine(surface);
+    }
+
+    @Override
+    protected void onDestroy() {
+        engine.stop();
     }
 }
